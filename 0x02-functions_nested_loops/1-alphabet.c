2aebdcbb0e9468c9d670251261d5b969c4a6cc1e
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-14 02:41:50 -0700

    using customised header to printout to stdio.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main- the starting point of application
+ *
+ * Return: 0 for success
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
+
+int main(void)
+{
+	print_alphabet(void);
+
+	return (0);
+}
