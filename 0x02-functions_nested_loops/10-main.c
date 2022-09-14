c6646e3a56f34f4ef545eaffbca12dd22649d58b
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-14 11:27:21 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - starting points of application
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
