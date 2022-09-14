9be96576c0805917831612c8b2425914f46c1229
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-14 07:57:01 -0700

    Absolute value for numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_abs - Check Description
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (i * -1);
+}
