c83d8ff140ad7f20eba79514687a2a7f35dc7d24
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-14 02:13:08 -0700

    using customised header to printout to stdio.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main-the starting point of applications
+ */
+int main()
+{
+	string input_[8] = "_inputchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_inputchar(input_[i]);
+	}
+
+	_inputchar('\n');
+	return (0);
+
+}
+
