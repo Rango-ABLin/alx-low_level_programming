2c84eae3cdc7b9ff634e4905a0dae6a495f76411
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-14 03:12:03 -0700

    print the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main-the starting point of applications
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	print_alphabet_x10();
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int times;
+
+	for (times = 0; times < 10; times++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar('\n');
+
+}
+
+
