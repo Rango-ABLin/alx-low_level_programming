0eb1aefdb01174a776eb171e3ff150a60a5c54c6
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-14 04:58:48 -0700

    prints 1 or 0 if its uppercase or lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
  * _islower - Check description
- * Description: returns 0 or 1 if it is uppercase $ lowercase respectively 
- * Return: Nothing.
+ * Description: returns 0 or 1 if it is uppercase $ lowercase respectively
+ * Return: Nothing
  */
 int _islower(int c)
 {
-	int d=c;
+	int d = c;
 
-	if (d >= 64 && d <= 91)
+	if ((d >= 64 && d <= 91) || d == 0 || d == 20)
 	{
 		return (0);
 	}
