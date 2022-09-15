0dab8c6fb25d6e1fc8d3841d12e144e34b18bd9c
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-14 18:48:58 -0700

    print natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@
 */
 void print_to_98(int n)
 {
-	if (n <=98)
+	if (n <= 98)
 	{
 		for (; n <= 98; n++)
 		{
@@ -24,10 +24,9 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; n >=98; n--)
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
-			
 			if (n == 98)
 				continue;
 			printf(", ");
