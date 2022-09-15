5fd71a55c23782e0e8c2f80cf1af9ddf4b4568a9
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-15 10:46:11 -0700

    debugging in C

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,9 +2,9 @@
 
 /**
  * main-the starting point of applications
- * Description: Positive or negative number proof
+ *
  * @num is for the input character
- * main takes in positive_or_negative function
+ * 
  * Return: 0 for success
  */
 int main(void)
