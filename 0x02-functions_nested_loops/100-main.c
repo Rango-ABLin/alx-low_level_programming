5f0952c6154231fb377785fb4e1665c4689db687
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-14 19:08:56 -0700

    timetable for numbers 0-14

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main-the starting poiint of application
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	return (0);
+}
