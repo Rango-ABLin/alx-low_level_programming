0e400b55da2f4f6c1ba6550b6a5cdfbeac483439
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-15 19:47:53 -0700

    Debugging in C

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main-the starting point of application
- * Return: 0 for success
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 int main(void)
 {
 	int num;
 
-	num = 98;
+	num = 0;
 	positive_or_negative(num);
 
 	return (0);
