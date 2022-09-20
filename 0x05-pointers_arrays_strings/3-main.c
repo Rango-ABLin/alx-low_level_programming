8b3423d09cc4135bba319fe057a97bc49cbb9012
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-20 07:45:30 -0700

    Pointers exercise 3

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: Alays 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "I do not fear computers. I fear lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
+}
