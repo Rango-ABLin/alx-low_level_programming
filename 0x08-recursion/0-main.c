a06a8a407e2fe813d12b4157da474f4654152ecf
Anthony Banda <anthonygeorgebanda@gmail.com>
2022-09-28 07:20:24 -0700

    ALX assignemtn on recursion

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - the entry point for C programs 
+ *
+ * Return: 0 for success 
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
